63278327c442fc8b66578fb200b2f8b200d295e5
Aizat Zuffri <aizatzuffri@gmail.com>
2024-03-24 02:15:00 +0800

    firebase storage + CRUD

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -2,10 +2,11 @@ import { useContext, useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap"
 import { useDispatch } from "react-redux";
 import { AuthContext } from "./AuthProvider";
-import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import { deletePost, likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import UpdatePostModal from "./UpdatePostModal";
 
 export default function ProfilePostCard({ post }) {
-    const { content, id: postId } = post;
+    const { content, id: postId, imageUrl } = post;
     const [likes, setLikes] = useState(post.likes || []);
     const dispatch = useDispatch();
     const { currentUser } = useContext(AuthContext);
@@ -15,6 +16,11 @@ export default function ProfilePostCard({ post }) {
     const isLiked = likes.includes(userId);
 
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+    const handleShowUpdateModal = () => setShowUpdateModal(true);
+    const handleCloseUpdateModal = () => setShowUpdateModal(false);
 
     const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
@@ -26,8 +32,15 @@ export default function ProfilePostCard({ post }) {
 
     // Remove userId from likes array and update the backend
     const removeFromLikes = () => {
+        // Optimistic like = immediate response to the client
+        // You will assume that the like is done, only then you pass the data to the db.
+        // If the like operation is not successful in the db, then it will revert back the state variable of likes.
         setLikes(likes.filter((id) => id !== userId));
         dispatch(removeLikeFromPost({ userId, postId }));
+    };
+
+    const handleDelete = () => {
+        dispatch(deletePost({ userId, postId }))
     };
 
     return (
@@ -46,6 +59,7 @@ export default function ProfilePostCard({ post }) {
                 <strong>Aizat</strong>
                 <span> @iceroll • Mar 13</span>
                 <p>{content}</p>
+                <Image src={imageUrl} style={{ width: 150 }} />
                 <div className="d-flex justify-content-between">
                     <Button variant="light">
                         <i className="bi bi-chat"></i>
@@ -67,6 +81,24 @@ export default function ProfilePostCard({ post }) {
                     <Button variant="light">
                         <i className="bi bi-upload"></i>
                     </Button>
+                    <Button variant="light">
+                        <i
+                            className="bi bi-pencil-square"
+                            onClick={handleShowUpdateModal}
+                        ></i>
+                    </Button>
+                    <Button variant="light">
+                        <i
+                            className="bi bi-trash"
+                            onClick={handleDelete}
+                        ></i>
+                    </Button>
+                    <UpdatePostModal
+                        show={showUpdateModal}
+                        handleClose={handleCloseUpdateModal}
+                        postId={postId}
+                        originalPostContent={content}
+                    />
                 </div>
             </Col>
         </Row>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import { collection, deleteDoc, doc, getDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
+import { db, storage } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 // Async thunk for fetching a user's posts
 export const fetchPostsByUser = createAsyncThunk(
@@ -23,23 +24,29 @@ export const fetchPostsByUser = createAsyncThunk(
     }
 );
 
+// Async thunk to create post
 export const savePost = createAsyncThunk(
     "posts/savePost",
-    async ({ userId, postContent }) => {
+    async ({ userId, postContent, file }) => {
         try {
-            const postRef = collection(db, `users/${userId}/posts`);
-            console.log(`users/${userId}/posts`);
+            let imageUrl = "";
+            console.log(file);
+            if (file !== null) {
+                const imageRef = ref(storage, `posts/${file.name}`);
+                const response = await uploadBytes(imageRef, file);
+                imageUrl = await getDownloadURL(response.ref);
+            }
+            const postsRef = collection(db, `users/${userId}/posts`);
+            // console.log(`users/${userId}/posts`);
             // Since no ID is given, Firestore auto generate a unique ID for this new document
-            const newPostRef = doc(postRef);
-            console.log(postContent);
-            await setDoc(newPostRef, { content: postContent, likes: [] });
+            const newPostRef = doc(postsRef);
+            await setDoc(newPostRef, { content: postContent, likes: [], imageUrl });
+            // console.log(postContent);
             const newPost = await getDoc(newPostRef);
-
             const post = {
                 id: newPost.id,
                 ...newPost.data(),
             };
-
             return post;
         } catch (error) {
             console.error(error);
@@ -48,6 +55,67 @@ export const savePost = createAsyncThunk(
     }
 );
 
+// Async thunk to update post
+export const updatePost = createAsyncThunk(
+    "posts/updatePost",
+    async ({ userId, postId, newPostContent, newFile }) => {
+        try {
+            // Upload the new file to the storage if it exists and get its URL
+            let newImageUrl;
+            if (newFile) {
+                const imageRef = ref(storage, `posts/${newFile.name}`);
+                const response = await uploadBytes(imageRef, newFile);
+                newImageUrl = await getDownloadURL(response.ref);
+            }
+            // Reference to the existing post
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+            // Get the current post data
+            const postSnap = await getDoc(postRef);
+            if (postSnap.exists()) {
+                const postData = postSnap.data();
+                // Update the post content and the image URL
+                const updatedData = {
+                    ...postData,
+                    content: newPostContent || postData.content,
+                    imageUrl: newImageUrl || postData.imageUrl,
+                };
+                // Update the existing document in Firestore
+                await updateDoc(postRef, updatedData);
+                // Return the post with updated data
+                const updatedPost = { id: postId, ...updatedData };
+                return updatedPost;
+            } else {
+                throw new Error("Post does not exist");
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+);
+
+// Async thunk to delete a post
+export const deletePost = createAsyncThunk(
+    "posts/deletePost",
+    async ({ userId, postId }) => {
+        try {
+            // 1. Get the post that you want to delete
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+
+            // 2. Actually deletes it
+            await deleteDoc(postRef)
+
+            // Return the ID of the deleted post
+            return postId; // action.payload
+
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+)
+
+// Async thunk to like post
 export const likePost = createAsyncThunk(
     "posts/likePost",
     async ({ userId, postId }) => {
@@ -71,6 +139,7 @@ export const likePost = createAsyncThunk(
     }
 );
 
+// Async thunk to remove like
 export const removeLikeFromPost = createAsyncThunk(
     "posts/removeLikeFromPost",
     async ({ userId, postId }) => {
@@ -126,6 +195,22 @@ const postsSlice = createSlice({
                         (id) => id !== userId``
                     );
                 }
+            })
+            .addCase(updatePost.fulfilled, (state, action) => {
+                const updatedPost = action.payload;
+                // Find and update the post in the state
+                const postIndex = state.posts.findIndex(
+                    (post) => post.id === updatedPost.id
+                );
+                if (postIndex !== -1) {
+                    state.posts[postIndex] = updatedPost;
+                }
+            })
+            .addCase(deletePost.fulfilled, (state, action) => {
+                const deletedPostId = action.payload;
+
+                // Filter out the deleted post from state
+                state.posts = state.posts.filter((post) => post.id !== deletedPostId);
             });
     },
 });
